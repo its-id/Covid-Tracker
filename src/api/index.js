fb0f5d81c0050ca0dce0355e07b78181cd314e5b
Indrakant Dana <ik.dana24@gmail.com>
2022-04-14 13:45:44 +0530

    Setup Basic Structure and Create Required Files.

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+const url = 'https://covid19.mathdro.id/api';
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Cards from "./Cards/cards";
+import Chart from "./Charts/chart";
+import CountryPicker from "./CountryPicker/countrypicker";
+
+export {
+    Cards,
+    Chart,
+    CountryPicker
+}
