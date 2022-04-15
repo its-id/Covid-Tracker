55f1789a2eb8868500c1faafe1b396afb759d7e5
Indrakant Dana <ik.dana24@gmail.com>
2022-04-15 14:19:27 +0530

    Update Cards.jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import styles from './app.module.css';
 
 import {Cards, Chart, CountryPicker} from './components';
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+
   render() {
+
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards/>
+        <Cards data={data}/>
         <Chart/>
         <CountryPicker/>
       </div>
